6fdafb59092b62f5646e9ed8c38909b71a777cfe
Abuzar <abuzarkhan1242@gmail.com>
2025-04-28 23:21:30 +0500

    done

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello!');
+})
+
+app.get('/api', (req, res) => {
+  res.send({ message: "Hello from AWS Backend!" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
